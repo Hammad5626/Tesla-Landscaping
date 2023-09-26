16d7693c0977503381b38954ca3d663b5b355642
Hamad Badar <Hamadbadar5626@hotmail.com>
2023-09-26 12:32:08 +0500

    allowed hosts

--- TeslaLandscaping/settings.py
+++ TeslaLandscaping/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+9gd)9a+qd^@x3o3i%6+)b(ewy_7&#uxa$#d9a6sstq86avz='
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
