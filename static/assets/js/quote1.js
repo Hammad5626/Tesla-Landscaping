071c3203a3129359e69d1dd1d2157fff24e125db
Hamad Badar <Hamadbadar5626@hotmail.com>
2023-10-11 11:55:36 +0500

    ...

--- static/assets/js/quote1.js
+++ static/assets/js/quote1.js
@@ -1,17 +1,21 @@
-function initializeAutocomplete() {
+document.addEventListener('DOMContentLoaded', function () {
     let input = document.getElementById('addressInput');
     let autocomplete = new google.maps.places.Autocomplete(input);
 
-    autocomplete.addListener('place_changed', function() {
+    autocomplete.addListener('place_changed', function () {
         let place = autocomplete.getPlace();
         if (!place.geometry) {
             console.log("No location details available for this place.");
             return;
         }
-        
+
         let lat = place.geometry.location.lat();
         let lng = place.geometry.location.lng();
-        window.location.href = "{% url 'quote2' %}?lat=" + lat + "&lng=" + lng;
+        
+        let url = input.getAttribute('data-url');
+        
+        let newUrl = `${url}?lat=${lat}&lng=${lng}`;
+        
+        window.location.href = newUrl;
     });
-}
-document.addEventListener('DOMContentLoaded', initializeAutocomplete);
+});
